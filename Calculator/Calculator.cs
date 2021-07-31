c01463f01867e1110389568a2bed74927d0e7f1b
Yuliya Anisko <tiranozavrraw@gmail.com>
2021-07-31 21:57:59 +0200

    Add project files.

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Calculator
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+
+        public Calculator(double a, double b)
+        {
+            A = a;
+            B = b;
+        }
+
+        public static bool CheckOperation(char operation)
+        {
+            if (operation.Equals('+') || operation.Equals('-') || operation.Equals('*') || operation.Equals('/'))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public double Calculate(char operation)
+        {
+            if (operation.Equals('+'))
+            {
+                return A + B;
+            }
+            if (operation.Equals('-'))
+            {
+                return A - B;
+            }
+            if (operation.Equals('*'))
+            {
+                return A * B;
+            }
+            if (operation.Equals('/'))
+            {
+                return A / B;
+            }
+            throw new ArgumentOutOfRangeException();
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var program = new Program();
+                program.Run(args);
+            }
+        }
+
+        void Run(string[] args)
+        {
+            Console.WriteLine("Please, enter two digits with space separator");
+            var input = Console.ReadLine();
+
+            Console.WriteLine("Please, enter operation. Valid operations are +, - , * , /");
+            var operation = Console.ReadLine();
+
+            var numbersString = input.Split(' ');
+            if (numbersString.Length != 2)
+            {
+                Console.WriteLine("Two numbers expected");
+                return;
+            }
+            
+            var numbers = numbersString.Select(x => double.Parse(x)).ToList();
+            Calculator calculator = new Calculator(numbers[0], numbers[1]);
+            if (Calculator.CheckOperation(char.Parse(operation)))
+            {
+                var result = calculator.Calculate(char.Parse(operation));
+                Console.WriteLine("Result " + result);
+            } else
+            {
+                Console.WriteLine("Result cannot be calculated");
+            }
+        }
+    }
+}
